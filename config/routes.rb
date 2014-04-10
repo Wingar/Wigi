141d48dd79178979f392dff5e5a4f69f66a43b40
Charlie Somerville <charlie@charliesomerville.com>
2014-04-11 01:52:36 +1000

    implement the git dump-http protocol

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   root 'pages#index'
   resources :pages
+
+  # git dump-http protocol:
+  get "/info/refs"          => "git_clone#refs"
+  get "/HEAD"               => "git_clone#head"
+  get "/objects/info/packs" => "git_clone#packs"
+  get "/objects/pack/:file" => "git_clone#pack_file"
+  get "/objects/*name"      => "git_clone#object"
 end
