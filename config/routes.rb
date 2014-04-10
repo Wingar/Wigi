43830082232466394b1ab452c248f175c42cdba7
Charlie Somerville <charlie@charliesomerville.com>
2014-04-11 02:14:29 +1000

    fix packfile route

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   get "/info/refs"          => "git_clone#refs"
   get "/HEAD"               => "git_clone#head"
   get "/objects/info/packs" => "git_clone#packs"
-  get "/objects/pack/:file" => "git_clone#pack_file"
+  get "/objects/pack/:file" => "git_clone#pack_file", format: false, file: /.+/
   get "/objects/*name"      => "git_clone#object"
 end
